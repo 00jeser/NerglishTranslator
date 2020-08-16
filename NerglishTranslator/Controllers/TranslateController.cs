42146f94320e5ffba72c6264448026649c9b0882
OOjeser <50872233+OOjeser@users.noreply.github.com>
2020-08-17 00:40:40 +0500

    start

---
+++ NerglishTranslator/Controllers/TranslateController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace NerglishTranslator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TranslateController : ControllerBase
+    {
+        private readonly ILogger<TranslateController> _logger;
+
+        public TranslateController(ILogger<TranslateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            string bodyStr;
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8, true, 1024, true))
+            {
+                bodyStr = reader.ReadToEndAsync().Result;
+            }
+            string rez = "";
+            foreach (string s in bodyStr.Split(' '))
+            {
+                int index = Static.en.IndexOf(s.ToLower());
+                if (index == -1)
+                    rez += s + " ";
+                else
+                    rez += Static.ner[index] + " ";
+            }
+            return rez;
+        }
+        [HttpPost]
+        public string Post()
+        {
+            string bodyStr;
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8, true, 1024, true))
+            {
+                bodyStr = reader.ReadToEndAsync().Result;
+            }
+            string rez = "";
+            foreach (string s in bodyStr.Split(' '))
+            {
+                int index = Static.en.IndexOf(s);
+                if (index == -1)
+                    rez += s + " ";
+                else
+                    rez += Static.ner[index] + " ";
+            }
+            return rez;
+        }
+    }
+}
---
+++ NerglishTranslator/Static.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerglishTranslator
+{
+    public static class Static
+    {
+        public static List<string> en = new List<string>(File.ReadAllLines("en.txt"));
+        public static List<string> ner = new List<string>(File.ReadAllLines("ner.txt"));
+    }
+}
